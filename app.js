7ad8240f1b2e72154a173ab31d69e26e4762d23e
Lucas-Jimenez-Uribe <uribelucas06@gmail.com>
2025-03-02 18:52:33 -0500

    desafio dia dos

--- app.js
+++ app.js
@@ -1,48 +1,19 @@
-let numeroUn = 1
-
-let stringUn = '1'
-
-let numeroTreinta = 30
-
-let stringTreinta = '30'
-
-let numeroDiez = 10
-
-let stringDiez = '10'
+var nombre = prompt("¿Cual es tu nombre? ")
+var edad = prompt("¿Que edad tines? ")
+var lenguaje = prompt("¿Que lenguaje estas aprendiendo? ")
+var respuestaUno = "si"
+var respuestaDos = "no"
 
 
 
 
- if (numeroUn == stringUn) {
 
-  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+alert(`Hola ${nombre}, tines ${edad} y estas aprendiendo ${lenguaje}`)
 
-} else {
+var respuesta = prompt(`¿Te gusta estuiar ${lenguaje} ?`) 
 
-  console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
-
-}
-
- 
-
-if ( numeroTreinta === stringTreinta) {
-
-  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
-
-} else {
-
-  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
-
-}
-
- 
-
-if (numeroDiez == stringDiez) {
-
-  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
-
-} else {
-
-  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
-
+if (respuesta == respuestaUno) {
+  alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.")
+}else{
+  alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?")
 }
