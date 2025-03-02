4f82a1abc6f9c2b608bafa6ffd67c824212e7306
Lucas-Jimenez-Uribe <uribelucas06@gmail.com>
2025-03-02 18:58:58 -0500

    dia dos v2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var nombre = prompt("¿Cual es tu nombre? ")
-var edad = prompt("¿Que edad tines? ")
+var edad = prompt("¿Que edad tiEnes? ")
 var lenguaje = prompt("¿Que lenguaje estas aprendiendo? ")
 var respuestaUno = "si"
 var respuestaDos = "no"
@@ -8,11 +8,11 @@ var respuestaDos = "no"
 
 
 
-alert(`Hola ${nombre}, tines ${edad} y estas aprendiendo ${lenguaje}`)
+alert(`Hola ${nombre}, tienes ${edad} y estas aprendiendo ${lenguaje}`)
 
-var respuesta = prompt(`¿Te gusta estuiar ${lenguaje} ?`) 
+var respuesta = prompt(`¿Te gusta estudiar ${lenguaje} ?`) 
 
-if (respuesta == respuestaUno) {
+if (respuesta.toLocaleLowerCase() === respuestaUno) {
   alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.")
 }else{
   alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?")
